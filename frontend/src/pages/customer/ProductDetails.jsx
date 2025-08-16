e10473c5c54cda537b36c6d4296632ec45f50d72
Dipanshu Kubde <dipanshukubde5678@gmail.com>
2025-08-16 18:17:05 +0530

    Rename ProductDetails to fix case sensitivity

--- frontend/src/pages/customer/ProductDetails.jsx
+++ frontend/src/pages/customer/ProductDetails.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./productdetails.css";
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
   const { id } = useParams();
